35233ef019c185849b2c2d4c0f694907e4bbca4a
svenakela <sven.wesley@gmail.com>
2023-10-30 11:02:47 +0100

    Caddy Community setup

--- db_backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p backups
-source conf/db.env
-FILE=backup-`date +%F`.sql
-
-docker compose exec db bash -c "/usr/bin/mariadb-backup --backup --user=root --password=$MYSQL_ROOT_PASSWORD --target-dir=tmp/bcp && tar czvf /tmp/$FILE.tar.gz /tmp/bcp && rm -rf /tmp/bcp"
-docker compose cp db:/tmp/$FILE.tar.gz ./backups
-docker compose exec db bash -c "rm -f /tmp/$FILE.tar.gz; rm -rf /tmp/bcp"
